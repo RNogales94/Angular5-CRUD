4574a4766ce7911f8d46397ba17937062c21fc07
RNogales <rnogales@correo.ugr.es>
2018-04-09 11:18:16 +0200

    Init repo

---
+++ mean-angular5/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Components
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+
+//Routes
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routerConfig';
+
+//Services
+import { CoinService } from './services/coin.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    CoinService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
